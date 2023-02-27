d663b7f4a6146564cfe74771f6c35d6182cf4337
whwstt <111332410+whwstt@users.noreply.github.com>
2023-02-27 09:50:20 +0800

    Update App.vue

--- 来源/App.vue
+++ 来源/App.vue
@@ -1,14 +1,6 @@
 <template>
   <div id="app">
     <router-view></router-view>
-<<<<<<< HEAD
-<<<<<<< HEAD:src/App.vue
-    <div></div>
-=======
-    <p></p>
->>>>>>> origin/master:来源/App.vue
-=======
->>>>>>> origin/master
   </div>
 </template>
 
